a113954c0579c2719f5e36cf6e056251f9352dad
bigboss424 <ajones061392@gmail.com>
2015-01-09 17:29:34 -0800

    Added the code to start the Test_BinarySearchDup assignment. As well as the SortSearchUtil and the utilities folder. I just need to google how to use packages again...

---
+++ Assignments/assign1/code/Test_BinarySearchDup.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test_BinarySearchDup
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to the Binary Search Duplicate Program!");
+
+		
+	}//end main
+
+}
---
+++ Assignments/assign1/code/utils/SortSearchUtil.java
@@ -0,0 +1,5 @@
+public class SortSearchUtil
+{
+
+
+}//end class
